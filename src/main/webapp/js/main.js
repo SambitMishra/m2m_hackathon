dc1caea84467c22ee56ab8ee0c14138e36ce11c0
SambitMishra <sambit.mishra001@gmail.com>
2015-08-02 02:15:59 -0700

    Integration is done

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -20,12 +20,18 @@ $(document).ready(function(){
 		$("#alertbox").hide();
 	});
 	$("#comparebtn").click(function(event) {
-		var inputJson = {
-			"companyList": [
-				"VZ"
-				
-			]
-		};
+		var selected = $('#company option:selected');
+		var companyList = [];
+		var total = selected.length;
+		
+		if(total != 0) {
+			for(var i=0; i < total; i++) {
+				companyList.push(selected[i].value);
+			}
+			var inputJson = {
+				"companyList": companyList
+			};
+		}
 		
 		/* Act on the event */
 		$.ajax({
@@ -34,104 +40,80 @@ $(document).ready(function(){
 			url: "secure/getCompaniesStockInfo",
 			method: 'POST',
 			// url: "resources/fixtures/getCompaniesStockInfo.json",
+			scope: companyList,
 			data: JSON.stringify(inputJson),
 			success: function(response){
-				console.log(JSON.stringify(response));
+				//Traverse data
+				var data = response.dataArray[0];
+				var output = [];
+				
+				for(var i=0; i < companyList.length; i++) {
+					var key = companyList[i];
+					output.push({
+						"code": data[key].symbol,
+						"companyName": data[key].name,
+						"price": data[key].quote.price,
+						"dayHigh": data[key].quote.dayHigh,
+						"dayLow": data[key].quote.dayLow,
+						"change": data[key].quote.change,
+					});
+				}
+				
+				drawChart(output);
 			}
 		});
-		var response = {
-		    "data": [
-		        {
-		            "code": "VZ",
-					"companyName": "Verizon",
-		            "price": 46.79,
-					"dayHigh": 47.17,
-					"dayLow": 46.71,
-					"change": 0.12,
-		        },
-		        {
-		            "code": "t",
-					"companyName": "AT & T",
-		            "price": 34.74,
-					"dayHigh": 34.99,
-					"dayLow": 34.72,
-					"change": -0.06,
-		        },
-		        {
-		            "code": "TMUS",
-					"companyName": "T-Mobile",
-		            "price": 40.66,
-					"dayHigh": 41.21,
-					"dayLow": 39.55,
-					"change": -0.06,
-		        },
-		        {
-		            "code": "USM",
-					"companyName": "US Cellular",
-		            "price": 37.22,
-					"dayHigh": 38.5,
-					"dayLow": 36.55,
-					"change": -0.06,
-		        }
-		    ],
-		    "success": true
-		};
-		var chart = new CanvasJS.Chart("chartContainer");
-		var data = response.data;
-		chart.options.title = { text: "NYSE" };
-		chart.options.data = [];
-		// chart.axisY.prefix = '$';
-		
-		var datapoints = [];
-		var seriesHigh = {
-	        type: "column"
-	    };
-		for(var i=0,len = data.length;i<len;i++){
-			var item = {
-				label : data[i].companyName,
-				y : data[i].dayHigh
-			}
-			datapoints.push(item);	
-		}
-		seriesHigh.dataPoints = datapoints;
-		chart.options.data.push(seriesHigh);
-		
-		var datapoints = [];
-		var seriesLow = {
-	        type: "column"
-	    };
-		for(var i=0,len = data.length;i<len;i++){
-			var item = {
-				label : data[i].companyName,
-				y : data[i].dayLow
-			}
-			datapoints.push(item);	
-		}
-		seriesLow.dataPoints = datapoints;
-		chart.options.data.push(seriesLow);
-		
-		var datapoints = [];
-		var seriesPrice = {
-	        type: "line"
-	    };
-		for(var i=0,len = data.length;i<len;i++){
-			var item = {
-				label : data[i].companyName,
-				y : data[i].price
-			}
-			datapoints.push(item);	
-		}
-		seriesPrice.dataPoints = datapoints;
-		chart.options.data.push(seriesPrice);
-		
-		chart.render();
-		drawChart(response);
 	});
 });
 
 function drawChart(data) {
+	var chart = new CanvasJS.Chart("chartContainer");
+	chart.options.title = { text: "NYSE" };
+	chart.options.data = [];
+	// chart.axisY.prefix = '$';
 	
-	alert('Chart is drawn');
+	var datapoints = [];
+	var seriesHigh = {
+		type: "column"
+	};
+	for(var i=0,len = data.length;i<len;i++){
+		var item = {
+			label : data[i].companyName,
+			y : data[i].dayHigh
+		}
+		datapoints.push(item);	
+	}
+	seriesHigh.dataPoints = datapoints;
+	chart.options.data.push(seriesHigh);
+	
+	var datapoints = [];
+	var seriesLow = {
+		type: "column"
+	};
+	for(var i=0,len = data.length;i<len;i++){
+		var item = {
+			label : data[i].companyName,
+			y : data[i].dayLow
+		}
+		datapoints.push(item);	
+	}
+	seriesLow.dataPoints = datapoints;
+	chart.options.data.push(seriesLow);
+	
+	var datapoints = [];
+	var seriesPrice = {
+		type: "line"
+	};
+	for(var i=0,len = data.length;i<len;i++){
+		var item = {
+			label : data[i].companyName,
+			y : data[i].price
+		}
+		datapoints.push(item);	
+	}
+	seriesPrice.dataPoints = datapoints;
+	chart.options.data.push(seriesPrice);
+	
+	chart.render();
 }
 
 
